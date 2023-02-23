fefcf29a2f6c86b706eb8a3151e51a5f9137b4fe
Insane <81003264+KingInsane46@users.noreply.github.com>
2023-02-23 15:20:49 -0600

    Add files via upload

--- Pong/src/Main.java
+++ Pong/src/Main.java
@@ -1,5 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.util.concurrent.TimeUnit;
 
 public class Main {
@@ -9,13 +11,15 @@ public class Main {
         GameObject
                 leftBlock = new GameObject(WORLD_SCALE,WORLD_SCALE*5, Color.white, true),
                 rightBlock = new GameObject(WORLD_SCALE,WORLD_SCALE*5, Color.white, true),
-                ball = new GameObject(WORLD_SCALE, WORLD_SCALE, Color.white, false);
+                ball = new GameObject(WORLD_SCALE, WORLD_SCALE, Color.white, true);
 
         leftBlock.setPosition(leftBlock.width/2, WORLD_SCALE*SCREEN_SIZE_HEIGHT/2);
         rightBlock.setPosition( WORLD_SCALE*SCREEN_SIZE_WIDTH- rightBlock.width, WORLD_SCALE*SCREEN_SIZE_HEIGHT/2);
         ball.setPosition(WORLD_SCALE*SCREEN_SIZE_WIDTH/2, WORLD_SCALE*SCREEN_SIZE_HEIGHT/2);
+
         JPanel panel = new JPanel()
         {
+            @Override
             public void paintComponent(Graphics g)
             {
                 super.paintComponent(g);
@@ -26,10 +30,11 @@ public class Main {
                 ball.draw(g);
             }
         };
-
+        KeyListener keyListener = new KeyListener();
         JFrame frame = new JFrame("Pong");
         frame.add(panel);
-        frame.setSize(WORLD_SCALE*SCREEN_SIZE_WIDTH, WORLD_SCALE*SCREEN_SIZE_HEIGHT);
+        frame.addKeyListener(keyListener);
+        frame.setSize(WORLD_SCALE*SCREEN_SIZE_WIDTH-10, WORLD_SCALE*SCREEN_SIZE_HEIGHT+15);
         frame.setResizable(false);
         frame.setLocationRelativeTo(null);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
@@ -42,6 +47,10 @@ public class Main {
             frame.repaint();
             ball.setPosition(ball.x + moveRightSpeed, ball.y + moveDownSpeed);
 
+            leftBlock.setPosition(leftBlock.x, leftBlock.y + keyListener.getLeftMoveDirection());
+            rightBlock.setPosition(rightBlock.x, rightBlock.y + keyListener.getRightMoveDirection());
+
+            //ball collision check
             collision = objectTouchingScreen(ball);
             if(collision == 1 || collision == 3)
                 moveRightSpeed *= -1;
@@ -59,13 +68,26 @@ public class Main {
                 moveRightSpeed *= -1;
             if(collision == 2 || collision == 4)
                 moveDownSpeed *= -1;
+
+            //block collision check with screen
+            collision = objectTouchingScreen(leftBlock);
+            if(collision == 2)
+                leftBlock.setPosition(leftBlock.x, leftBlock.y - keyListener.MOVE_SPEED);
+            if(collision == 4)
+                leftBlock.setPosition(leftBlock.x, leftBlock.y + keyListener.MOVE_SPEED);
+
+            collision = objectTouchingScreen(rightBlock);
+            if(collision == 2)
+                rightBlock.setPosition(rightBlock.x, rightBlock.y - keyListener.MOVE_SPEED);
+            if(collision == 4)
+                rightBlock.setPosition(rightBlock.x, rightBlock.y + keyListener.MOVE_SPEED);
         }
     }
 
     static public int objectTouchingScreen(GameObject obj)
     {
-        int     left = 0,
-                top = 0,
+        int     left = 25,
+                top = 25,
                 right = WORLD_SCALE*SCREEN_SIZE_WIDTH,
                 bottom = WORLD_SCALE*SCREEN_SIZE_HEIGHT,
 
@@ -133,6 +155,55 @@ class GameObject
             return 2;
         if(left < objRight && right > objRight && top < objBottom && bottom > objBottom)
             return 4;
+        return 0;
+    }
+}
+
+class KeyListener extends KeyAdapter
+{
+    final public int MOVE_SPEED = 2;
+    final int W = 87, S = 83, UP_ARROW = 38, DOWN_ARROW = 40;
+    boolean wDown = false, sDown = false, arrowUpDown = false, arrowDownDown = false;
+
+    @Override
+    public void keyPressed(KeyEvent event)
+    {
+        switch(event.getKeyCode())
+        {
+            case W -> wDown = true;
+            case S -> sDown = true;
+            case UP_ARROW -> arrowUpDown = true;
+            case DOWN_ARROW -> arrowDownDown = true;
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent event)
+    {
+        switch(event.getKeyCode())
+        {
+            case W -> wDown = false;
+            case S -> sDown = false;
+            case UP_ARROW -> arrowUpDown = false;
+            case DOWN_ARROW -> arrowDownDown = false;
+        }
+    }
+
+    public int getLeftMoveDirection()
+    {
+        if(wDown)
+            return -MOVE_SPEED;
+        if(sDown)
+            return MOVE_SPEED;
+        return 0;
+    }
+
+    public int getRightMoveDirection()
+    {
+        if(arrowUpDown)
+            return -MOVE_SPEED;
+        if(arrowDownDown)
+            return MOVE_SPEED;
         return 0;
     }
 }
